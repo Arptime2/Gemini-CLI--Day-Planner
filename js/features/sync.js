7806fe320ea202af06baeea0f2e1a10e26f6d251
Arptime2 <p.kohlstrunk@gmx.de>
2025-07-21 19:06:10 +0200

    fix sync

--- js/features/sync.js
+++ js/features/sync.js
@@ -1,7 +1,14 @@
 class WebRTCSync {
     constructor(onDataReceived) {
         this.peerConnection = new RTCPeerConnection({
-            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+            iceServers: [
+                { urls: 'stun:stun.l.google.com:19302' },
+                {
+                    urls: 'turn:openrelay.metered.ca:80',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                }
+            ]
         });
         this.dataChannel = null;
         this.onDataReceived = onDataReceived;
